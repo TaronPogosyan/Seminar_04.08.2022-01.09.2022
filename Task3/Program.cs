19332c3247547f3c30b58359069e9792be431473
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-06 22:46:35 +0300

    Задача №5-1

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,6 +8,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 int num2 = -num;
 while (num2 <= num)
 {
-    Console.Write(num2 + "; ");
-    num2 ++;
+    Console.Write(num2 + ", ");
+    num2++;
 }
+Console.WriteLine();
