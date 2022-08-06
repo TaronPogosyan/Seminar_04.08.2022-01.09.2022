5118b86b6d2a073b9fe85df97793fbec51af784f
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-06 22:41:18 +0300

    Задача №5

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,4 +2,12 @@
 // а на выходе показывает все целые числа в промежутке от -N до N
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> "-2, -1, 0, 1, 2"
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+int num2 = -num;
+while (num2 <= num)
+{
+    Console.Write(num2 + "; ");
+    num2 ++;
+}
