ce21597a7462ffceca83a76c26cd162ec24bbeb6
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-06 23:22:56 +0300

    Задача №7 добавил второй вариант

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,8 +4,24 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
+
+// Вариант 1
 Console.Write("Напишите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine()); 
 int num2 = num % 10;
 Console.WriteLine($"Показывает последнюю цифру: {num2}");
+
+// Вариант 2
+Console.Write("Введите 3-х значное число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 0)
+{
+    Console.WriteLine($"Крайняя цифра введенного числа: {a * -1 % 10}");
+}
+else
+{
+    Console.WriteLine($"Крайняя цифра введенного числа: {a % 10}");
+}
+
  
